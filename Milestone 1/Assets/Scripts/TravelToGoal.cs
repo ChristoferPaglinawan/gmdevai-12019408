d279553972a9a10f5dc28c9d45a0a614ecb586fb
ChristoferPaglinawan <christoferjohn.paglinawan@benilde.edu.ph>
2023-06-09 23:45:35 +0800

    Adjusted the repository.

---
+++ Milestone 1/Assets/Scripts/TravelToGoal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravelToGoal : MonoBehaviour
+{
+    public Transform goal;
+    float speed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 direction = goal.position - this.transform.position;
+        transform.LookAt(goal);
+        if(direction.magnitude > 1)
+        {
+            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
